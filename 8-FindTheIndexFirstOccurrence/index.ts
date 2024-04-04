dc4a78554f7b2e01945ae9f23a9cdc6d7563eb0e
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-04-04 15:26:42 +0300

    find Index

--- 8-FindTheIndexFirstOccurrence/index.ts
+++ 8-FindTheIndexFirstOccurrence/index.ts
@@ -3,37 +3,13 @@ function strStr(haystack: string, needle: string): number {
   // return haystack.indexOf(needle)
 
   //but we aren't looking for easy ways
-  let iter = 0;
-  let firstIndex = -1;
+  let m = haystack.length;
+  let n = needle.length;
 
-  for (let i = 0; i < haystack.length; i++) {
-    if (iter === 0 && haystack.slice(i).length < needle.slice(iter).length) {
-      return -1;
-    }
+  for (let i = 0; i < m - n + 1; ++i)
+    if (haystack.slice(i, i + n) === needle) return i;
 
-    if (haystack[i] === needle[iter]) {
-      console.log('trueee');
-      if (iter === 0) {
-        firstIndex = i;
-      }
-
-      if (iter === needle.length - 1) {
-        return firstIndex;
-      }
-
-      iter += 1;
-    } else {
-      if (iter > 0) {
-        console.log('falssse');
-        i = i - iter + 1;
-        iter = 0;
-      }
-
-      firstIndex = -1;
-    }
-  }
-
-  return firstIndex;
+  return -1;
 }
 
 console.log('test 1: sadbutsad');
@@ -43,7 +19,7 @@ console.log('test 2: leetcode');
 console.log(-1 === strStr('leetcode', 'leeto'));
 
 console.log('test 3: mississippi');
-console.log(strStr('mississippi', 'pi'));
+console.log(9 === strStr('mississippi', 'pi'));
 
 console.log('test 3: aaa');
 console.log(0 === strStr('aaa', 'aaa'));
