4da5a9fe384ea75f4d44e26eaba7d6584a25632f
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-04-09 20:13:24 +0300

    plus one

---
+++ 14-PlusOne/index.ts
@@ -0,0 +1,12 @@
+function plusOne(digits: number[]): number[] {
+  const n = digits.length;
+
+  for (let i = n - 1; i >= 0; i--) {
+      if (10 > ++digits[i]) {
+          return digits;
+      }
+      digits[i] %= 10;
+  }
+  
+  return [1, ...digits];
+}
