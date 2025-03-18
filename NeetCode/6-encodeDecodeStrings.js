61665529a49ff0c3291bd8f092a375f0cc8d7a0b
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-03-18 11:37:36 -0400

    solution

--- NeetCode/6-encodeDecodeStrings.js
+++ NeetCode/6-encodeDecodeStrings.js
@@ -1,48 +1,34 @@
-function encode(strs) {
-  let result = '';
-
-  if (strs.length === 0) {
-    return null;
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @returns {string}
+   */
+  encode(strs) {
+    let res = '';
+    for (let s of strs) {
+      res += s.length + '#' + s;
+    }
+    return res;
   }
 
-  if (strs.length === 1 && strs[0] === '') {
-    return '';
+  /**
+   * @param {string} str
+   * @returns {string[]}
+   */
+  decode(str) {
+    let res = [];
+    let i = 0;
+    while (i < str.length) {
+      let j = i;
+      while (str[j] !== '#') {
+        j++;
+      }
+      let length = parseInt(str.substring(i, j));
+      i = j + 1;
+      j = i + length;
+      res.push(str.substring(i, j));
+      i = j;
+    }
+    return res;
   }
-
-  for (let i = 0; i < strs.length; i++) {
-    let lenWord = strs[i].length;
-    result += `(${lenWord})` + strs[i];
-  }
-
-  console.log(result);
-
-  return result;
 }
-
-function decode(str) {
-  let result = [];
-
-  if (str === null) {
-    return [];
-  }
-
-  if (str === '') {
-    return [''];
-  }
-
-  for (let i = 0; i < str.length - 3; i++) {
-    endOfLen = str.indexOf(')');
-    let lenN = str.slice(i + 1, endOfLen);
-    console.log(lenN);
-
-    let lenNum = Number(str[i + 1]);
-    //error because number can be more than 1 symbol. Count it and use ()
-    result.push(str.slice(i + 1, i + 1 + lenNum));
-    i = i + lenNum;
-  }
-  return result;
-}
-
-let line = encode(['wgfdsahgfde', 'say', ':', 'yesf', '!@#$%^&*()']);
-
-console.log(decode(line));
