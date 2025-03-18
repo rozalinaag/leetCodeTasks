4baec91ce8710f4b7240482605858a3a76e00d8a
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-03-17 21:00:29 -0400

    slice

--- NeetCode/6-encodeDecodeStrings.js
+++ NeetCode/6-encodeDecodeStrings.js
@@ -11,7 +11,7 @@ function encode(strs) {
 
   for (let i = 0; i < strs.length; i++) {
     let lenWord = strs[i].length;
-    result += `${lenWord}` + strs[i];
+    result += `(${lenWord})` + strs[i];
   }
 
   console.log(result);
@@ -31,18 +31,18 @@ function decode(str) {
   }
 
   for (let i = 0; i < str.length - 3; i++) {
-    if (Number(str[i])) {
-      let lenNum = Number(str[i]);
-      //error because number can be more than 1 symbol. Count it and use ()
-      console.log(lenNum);
-      console.log(str.slice(i + 1, i + 1 + lenNum));
-      result.push(str.slice(i + 1, i + 1 + lenNum));
-      i = i + lenNum;
-    }
+    endOfLen = str.indexOf(')');
+    let lenN = str.slice(i + 1, endOfLen);
+    console.log(lenN);
+
+    let lenNum = Number(str[i + 1]);
+    //error because number can be more than 1 symbol. Count it and use ()
+    result.push(str.slice(i + 1, i + 1 + lenNum));
+    i = i + lenNum;
   }
   return result;
 }
 
-let line = encode(['we', 'say', ':', 'yesf', '!@#$%^&*()']);
+let line = encode(['wgfdsahgfde', 'say', ':', 'yesf', '!@#$%^&*()']);
 
 console.log(decode(line));
