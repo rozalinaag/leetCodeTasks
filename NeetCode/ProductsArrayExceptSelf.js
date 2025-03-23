59e5792056f6e341fd5b061acf582df580e24740
rozalinaaag <rosalina2000@mail.ru>
2025-03-23 18:46:08 -0400

    prefix postfix solution

--- NeetCode/ProductsArrayExceptSelf.js
+++ NeetCode/ProductsArrayExceptSelf.js
@@ -1,6 +1,18 @@
 function productExceptSelf(nums){
-    let result = []
+    let result = Array(nums.length).fill(1);
 
+    let prefix = 1
+    for (let i = 1; i < nums.length; i++) {
+        result[i] = prefix;
+        prefix *=nums[i]
+    }
 
+    let postfix = 1
+    for (let i = nums.length - 1; i > -1; --i) {
+        result[i] *=postfix;
+        postfix *=nums[i]
+    }
     return result
 }
+
+console.log(productExceptSelf([1,2,4,6]))
