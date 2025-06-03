55bd8ee338689b1cfad54496c61936a506e6a5eb
rozalinaaag <rosalina2000@mail.ru>
2025-06-03 14:00:10 -0400

    clone array - slice(0)

--- practice/arrays.js
+++ practice/arrays.js
@@ -9,3 +9,13 @@ function is_array(arr){
 console.log(is_array(["3", "4", "5"]))
 console.log(is_array('hfrdjs'))
 
+function array_clone(arr){
+    return arr.slice(0)
+}
+
+let test1 = [1, 3, 4]
+let test2 = array_clone(test1)
+
+test1.push(5)
+
+console.log(test1, test2)
