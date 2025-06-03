cee73dd03fc919f50156e6bad5c7128a895f88ca
rozalinaaag <rosalina2000@mail.ru>
2025-06-03 14:04:16 -0400

    join arrays

--- practice/arrays.js
+++ practice/arrays.js
@@ -9,6 +9,8 @@ function is_array(arr){
 console.log(is_array(["3", "4", "5"]))
 console.log(is_array('hfrdjs'))
 
+//------------------------------------------------------------
+
 function array_clone(arr){
     return arr.slice(0)
 }
@@ -19,3 +21,21 @@ let test2 = array_clone(test1)
 test1.push(5)
 
 console.log(test1, test2)
+
+//-------------------------------------------------------------
+
+function firstElementsOfArray(arr, number){
+    return arr.slice(0, number)
+}
+
+console.log(firstElementsOfArray([1,2,3,4], 3))
+
+//-------------------------------------------------------------
+
+function joinElementsOfArray(arr, arr2){
+    let newArr = arr.slice(0)
+    newArr.push(...arr2)
+    return newArr
+}
+
+console.log(joinElementsOfArray([2,4,5], [5, 3]))
